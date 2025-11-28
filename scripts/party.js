dca925d51d149536a25985923cd287f823f3204e
TypeLuis <typeluis@gmail.com>
2025-11-28 18:46:11 +0000

    made modal dynamic

--- scripts/party.js
+++ scripts/party.js
@@ -12,13 +12,24 @@
 let parties = JSON.parse(localStorage.getItem("parties") || "[]");
 console.log(parties)
 
-// form
+
+// Elements
+const modal = document.querySelector("#myModal")
+const button = document.querySelector('#modal_button')
+const close = document.querySelector(".close")
+const modal_content = document.querySelector(".modal-content")
+
 const form = document.querySelector('form')
 const episode = document.querySelector('input[type="number"]');
 const date = document.querySelector('input[type="date"]')
 const name = document.querySelector('input[type="text"]')
 const time = document.querySelector('input[type="time"]')
 const selection = document.querySelector('select')
+
+
+
+// form
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -37,6 +48,20 @@ form.addEventListener("submit", (e) => {
         episode : episode.value,
         ...dateObj // spread dateObj to current Obj
     }
+    
+    name.value = ""
+    date.value = ""
+    episode.value = ""
+    time.value = ""
+    selection.value = ""
+
+    const element = document.createElement("p")
+    element.textContent = "Form submitted sucessfully"
+    modal_content.appendChild(element)
+
+    modal.style.display = "block"
+    modal_content.style.top = 0
+    requestAnimationFrame(() =>  modal_content.style.top = "50%")
 
     parties.push(obj);
 
@@ -56,13 +81,24 @@ episode.addEventListener("keydown", (e) => {
 
 
 
+
 // Modal
-const modal = document.querySelector("#myModal")
-const button = document.querySelector('#modal_button')
-const close = document.querySelector(".close")
-const modal_content = document.querySelector(".modal-content")
+
+const party_list = data => {
+    const el = document.createElement("li")
+    el.textContent = data
+    return el
+}
 
 button.addEventListener("click", () => {
+    const ulEl = document.createElement('ul')
+    ulEl.appendChild(party_list("Lorem"))
+    ulEl.appendChild(party_list("Lorem, ipsum dolor."))
+    ulEl.appendChild(party_list("Lorem ipsum dolor sit amet"))
+    ulEl.appendChild(party_list("Lorem, ipsum"))
+    ulEl.appendChild(party_list("Lorem ipsum dolor sit"))
+    modal_content.appendChild(ulEl)
+
     modal.style.display = "block"
     modal_content.style.top = 0
     requestAnimationFrame(() =>  modal_content.style.top = "50%")
@@ -70,15 +106,17 @@ button.addEventListener("click", () => {
 
 close.addEventListener("click", () => {
     modal_content.style.top = 0
-    modal_content.addEventListener("transitionend", () => modal.style.display = "none", {once: true})
+    modal_content.addEventListener("transitionend", () => {
+        modal.style.display = "none"
+        document.querySelector("ul") ? modal_content.removeChild(document.querySelector('ul')) : modal_content.removeChild(document.querySelector('p'))
+    }, {once: true})
 })
 
 window.addEventListener("click", (e) => {
     if (e.target == modal && modal.style.display === "block"){
         modal_content.style.top = 0
         modal.style.display = "none"
+        document.querySelector("ul") ? modal_content.removeChild(document.querySelector('ul')) : modal_content.removeChild(document.querySelector('p'))
     } 
 })
-
-
 
